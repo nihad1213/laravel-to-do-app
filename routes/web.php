8aa3891cc15057800dd406957e99bbdaec6ce0dd
nihad1213 <nihad.nemetli@gmail.com>
2024-11-19 02:47:19 +0400

    Register part created

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
-use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,5 +12,6 @@ Route::get('/', function () {
 
 Route::prefix('auth')->group(function() {
     Route::post('/login', LoginController::class);
-    
+    Route::post('/logout', LogoutController::class);
+    Route::post('/register', RegisterController::class);
 });
